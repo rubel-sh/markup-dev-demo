52e0bd88a2c42f14565867a7a9fab2166857bf8a
Rubel Hossain <rubel.hossain0649@gmail.com>
2024-03-10 12:19:52 +0600

    added animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded Scripts");
-});
+const slideUp = {
+  distance: "30%",
+  origin: "bottom",
+  opacity: "0%",
+  cleanup: false,
+};
+
+const sectionsArray = [
+  document.querySelector("#resume-section"),
+  document.querySelector("#skill-section"),
+  document.querySelector("#portfolio-section"),
+  document.querySelector("#blog-section"),
+  document.querySelector("#pricing-section"),
+  document.querySelector("#contact-section"),
+];
+
+ScrollReveal().reveal(sectionsArray, slideUp);
